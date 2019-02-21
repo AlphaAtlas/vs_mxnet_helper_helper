d3f047f1f6bc2b6ba2151a4aa10af64bdac1d7b7
AlphaAtlas <46462706+AlphaAtlas@users.noreply.github.com>
2019-02-21 13:58:37 -0600

    Update DownloadVapoursynth.bat

--- Scripts/DownloadVapoursynth.bat
+++ Scripts/DownloadVapoursynth.bat
@@ -22,10 +22,13 @@ set /p VSURL=<url.txt
 del url.txt
 echo Downloading a ~100MB archive from %VSURL%...
 powershell -Command "Import-Module BitsTransfer; Start-BitsTransfer %VSURL% fatpack.7z"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/AlphaAtlas/vs_mxnet_helper/archive/master.zip', 'vs_mxnet_helper-master.zip')"
 echo Downloading 7zip...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/AlphaAtlas/vs_mxnet_helper_helper/raw/master/7za.exe', '7za.exe')"
 echo Extracting portable Vapoursynth pack...
-7za.exe x fatpack.7z -o..
+7za.exe x fatpack.7z -o.. -aoa
+7za.exe x vs_mxnet_helper-master.zip 
+robocopy vs_mxnet_helper-master ..\VapourSynth64Portable /e /MOV /XO
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/AlphaAtlas/vs_mxnet_helper_helper/master/Git_URL', 'url.txt')"
 set /p GitURL=<url.txt
 del url.txt
@@ -33,10 +36,11 @@ echo Downloading a ~100MB portable Git archive from %GitURL%...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%GitURL%', 'PortableGit.exe')"
 echo Extracting git...
 mkdir ..\VapourSynth64Portable\PortableGit
-7za.exe x PortableGit.exe -o..\VapourSynth64Portable\PortableGit
+7za.exe x PortableGit.exe -o..\VapourSynth64Portable\PortableGit -aoa
 cd ..\VapourSynth64Portable\PortableGit
 start /wait "Post-Install" cmd /c post-install.bat
 cd ..
+start /wait "Even More Installing" cmd /c _install_and_update_everything.bat
 echo Compressing...
 compact /c /s * /i /q
 echo Cleaning up.
